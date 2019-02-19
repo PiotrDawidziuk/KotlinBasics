5f7233afbb1b0ff397eb5e2229920c8bfbae9b0f
PiotrDawidziuk <piotr.k.dawidziuk@gmail.com>
2019-02-19 10:28:58 +0100

    variables 1

--- src/HelloWorld.kt
+++ src/HelloWorld.kt
@@ -1,3 +1,14 @@
+import com.sun.org.apache.xml.internal.dtm.ref.sax2dtm.SAX2DTM2
+
 fun main(args: Array<String>) {
     println("Hello World!")
+
+    var age1: Int = 30
+    var age2 = 32
+
+    var name = "Pedro"
+
+    var sentence = "My name is $name and I am $age1 years old."
+
+    println(sentence)
 }
