1f2cc339635adb8062497d1e2c51c6292ee59715
PiotrDawidziuk <piotr.k.dawidziuk@gmail.com>
2019-02-19 11:23:23 +0100

    loops

---
+++ src/Loops.kt
@@ -0,0 +1,19 @@
+fun main(args: Array<String>) {
+
+    for (x in 1..10){
+        println(x)
+    }
+
+    var users = listOf("user1","user2","user3")
+
+    for (user in users) {
+        println(user)
+    }
+
+    for (num in 1..200){
+        if (num % 2 !   = 0){
+            println(num)
+        }
+    }
+
+}
